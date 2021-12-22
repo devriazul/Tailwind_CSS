dc910a939b428c1fac593a49a6db16d43145a22f
devriazul <engr.riazul@gmail.com>
2021-12-23 00:12:27 +0600

    Error found

---
+++ test-react-tailwind/test-tailwindcss/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-center text-red-500">Hello World</h1>
+      <h1 className="text-6xl">V3 working on react js</h1>
+      <p>2.3diye kaj chalai nen. jokhn production build hobe tokhn mode e theke jit enable kore diben</p>
+    </div>
+  );
+}
+
+export default App;
